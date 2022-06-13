44b64c82af8fa3342a3ee9934c161897e1a9e8d2
Beth-Anne Farmer <bethannef@protonmail.com>
2022-06-13 12:51:03 -0400

    goal app

---
+++ Native/App.js
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import { 
+  StyleSheet,  
+  View, 
+  FlatList
+} from 'react-native';
+
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
+
+export default function App() {
+
+const [courseGoals, setCourseGoals] = useState([]);
+
+function addGoalHandler(enteredGoalText) {
+  setCourseGoals(currentCourseGoals => [
+    ...currentCourseGoals, 
+      {text: enteredGoalText, id: Math.random().toString()},
+    ]);
+}
+
+function deleteGoalHandler(id) {
+  setCourseGoals((currentCourseGoals) => {
+    return currentCourseGoals.filter((goal) => goal.id !== id);
+  });
+}
+  return (
+    <View style={styles.appContainer}>
+   <GoalInput onAddGoal={addGoalHandler} />
+    <View style={styles.goalsContainer}>
+    <FlatList 
+    data={courseGoals} 
+    renderItem={(itemData) => {
+         return (
+         <GoalItem 
+         text={itemData.item.text} 
+         id={itemData.item.id}
+         onDeleteItem={deleteGoalHandler}
+         />
+         );
+    }}
+    keyExtractor={(item, index) => {
+          return item.id;
+    }}
+    alwaysBounceVertical={false} 
+    />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+    paddingTop: 50,
+    paddingHorizontal: 16,
+    flex: 1
+  },
+  goalsContainer: {
+    flex: 5
+  },
+});
